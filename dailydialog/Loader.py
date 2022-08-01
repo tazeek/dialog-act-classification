cd77a5e6632704305c81ce5fed32924bdf5c6dcb
tazeek <tazeek.rakib@gmail.com>
2022-08-01 14:41:32 +0800

    Load utterances

--- dailydialog/Loader.py
+++ dailydialog/Loader.py
@@ -10,8 +10,20 @@ class Loader:
 
         # Perform mapping
 
-        def _load_dialogues(self) -> list:
-            pass
+        def _load_dialogues(self) -> dict:
+            
+            utterances_dict = {}
+
+            with open('data\dialogues_text.txt', encoding='utf-8') as f:
+
+                for index, line in enumerate(f.readlines()):
+
+                    # Replace the un-processed character
+                    # and remove newline character
+                    line = line.replace('’',"'").rstrip('\n')
+                    utterances_dict[index] = line.split('__eou__')
+
+            return utterances_dict
 
         def _load_act_labels(self) -> list:
             pass
