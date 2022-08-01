4b59bbd85c4399b1d577cc5d23a6d7d17e5794e3
tazeek <tazeek.rakib@gmail.com>
2022-08-01 13:39:52 +0800

    Pseudocode added

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 if __name__ == '__main__':
-    pass
+    
+    # Load the dataset
+    # Get the dataframe correctly
+    # Should be:
+    # - Label: inform, question, etc
+    # - Utterance: full sentence
+
+    # Prepare the language model
+    # Choice of: XLNet, BERT, ConceptNet, RoBERTa
+
+    # Pre-process the data
+
+    # Create a ML model
+
+    # Portion out the dataset into: train, valid, test
+
+    # Train the model
