28dd9e9ca480bd12df301f2c86d24a05f516c15e
tazeek <tazeek.rakib@gmail.com>
2022-08-01 14:36:43 +0800

    Pseudocode added

--- dailydialog/Loader.py
+++ dailydialog/Loader.py
@@ -1,4 +1,20 @@
 class Loader:
 
     def __init__(self):
-        pass
+        
+        # Load dialoges
+        # One dialogue: many utterances
+
+        # Load act annotations
+        # One dialogue: many act
+
+        # Perform mapping
+
+        def _load_dialogues(self) -> list:
+            pass
+
+        def _load_act_labels(self) -> list:
+            pass
+
+        def _map_utter_act(self) -> None:
+            pass
