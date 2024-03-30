0c56763411e25bb9a6e7b1ccc0702b09558b5728
tazeek <tazeek.rakib@gmail.com>
2024-03-30 23:29:27 +0800

    First step done

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
 
+# Load the hyperparameter settings
+
+# Load the dataset
+# Batch and transform accordingly
+# Dimension: B * L_C * L_U
+# - B: Batch Size
+# - L_C: Length of a conversation
+# - L_U: Length of an utterance
+
 
