836cf5f0e008ad21acee021b91771d7ccba82fa0
tazeek <tazeek.rakib@gmail.com>
2024-03-30 23:31:37 +0800

    Flow added

--- main.py
+++ main.py
@@ -9,4 +9,15 @@
 # - L_C: Length of a conversation
 # - L_U: Length of an utterance
 
+# Begin epoch iterations
+
+# Flow:
+# 1. Transform with BERT: Raw -> CLS
+# 2. Feed into Capsule Nets: CLS -> Features
+# 3. Feed into two attentions: Utterance and Context
+# 4. Perform feature fusion
+# 5. Use linear layer for prediction
+
+# Load the BERT model and transform the data
+
 
